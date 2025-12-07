34a512c21409c6e9b64f58f639f391f5e37161f3
sandiproy8536-creator <sandiproy8536@gmail.com>
2025-12-07 11:43:12 +0530

    Update add.py

--- GITHUB/Sandip-Demo/add.py
+++ GITHUB/Sandip-Demo/add.py
@@ -1,4 +1,9 @@
 a = 5
 b = 8
 sum = a+b 
+
 print ("The sum of a and b is :",sum) 
+s = int(input("Enter first number: "))
+d = int(input("Enter first number: "))
+mul = s*d
+print("The Multiplication of s and d is",mul)
